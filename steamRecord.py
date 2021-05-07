28a2a63dc57887bd4370db669efa619aa47eef96
Jakub Kohoutek <62189043+JustAKuba@users.noreply.github.com>
2021-05-07 21:46:15 +0100

    WIP loading data from Steam Web API

--- steamRecord.py
+++ steamRecord.py
@@ -1,22 +1,23 @@
-from urllib.request import urlopen  
+from urllib.request import urlopen
 from pathlib import Path
 import re
 import datetime
 import time
+import json
 
 def monthToNumber(month):
-#Converts three char month to number
-	try:
-		return time.strptime(month, '%b').tm_mon 
-	except:
-		return 0
+    """Converts three char month to number"""
+    try:
+        return time.strptime(month, '%b').tm_mon 
+    except:
+        return 0
 
 
 class Game:
     
     def __init__(self,steam_id):
         "Represents data object of a game"
-        self.steam_id = steam_id
+        self.steam_id = str(steam_id)
         self.name = ""
         self.price = 0.0
         self.publisher = ""
@@ -26,7 +27,33 @@ class Game:
     def update(self):
         "Updates information about the game"
         
-        
+        req = urlopen('https://store.steampowered.com/api/appdetails?appids=' + str(self.steam_id))
+        rawData = json.load(req)
+        gameData = rawData[self.steam_id]["data"]
+
+        """
+        data = requests.get('https://store.steampowered.com/api/appdetails?appids=' + str(self.steam_id))
+        gameData = data[self.steam_id]["data"]
+        """
+
+        self.name = gameData["name"]
+        self.price = gameData["package_groups"][0]["subs"][0]["price_in_cents_with_discount"]
+        self.price = self.price/100
+        self.publisher = gameData["publishers"][0]
+        self.developer = gameData["developers"][0]
+        tempReleaseDate = gameData["release_date"]["date"]
+        tempReleaseDate = re.sub(r'[^\w]', ' ', tempReleaseDate).split(" ")
+        numMonth = monthToNumber(tempReleaseDate[1])
+        self.releaseDate = datetime.date(int (tempReleaseDate[3], numMonth, tempReleaseDate[0]))
+
+        print(self.name)
+        print(self.price)
+        print(self.publisher)
+        print(self.developer)
+        print(self.releaseDate)
+
+
+
         
         
         
@@ -60,3 +87,7 @@ class Game:
         self.releaseDate = datetime.date(int (updateTime[3], int (monthToNumber(updateTime[1])), int (updateTime[0]))
         """
 
+
+
+deb = Game(107410)
+deb.update()
