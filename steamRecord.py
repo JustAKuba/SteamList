6cc35f38b83134e4d26a93eaa895240a2efdc41c
Jakub Kohoutek <62189043+JustAKuba@users.noreply.github.com>
2021-05-07 21:50:57 +0100

    lul

--- steamRecord.py
+++ steamRecord.py
@@ -31,10 +31,7 @@ class Game:
         rawData = json.load(req)
         gameData = rawData[self.steam_id]["data"]
 
-        """
-        data = requests.get('https://store.steampowered.com/api/appdetails?appids=' + str(self.steam_id))
-        gameData = data[self.steam_id]["data"]
-        """
+
 
         self.name = gameData["name"]
         self.price = gameData["package_groups"][0]["subs"][0]["price_in_cents_with_discount"]
@@ -51,42 +48,6 @@ class Game:
         print(self.publisher)
         print(self.developer)
         print(self.releaseDate)
-
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        """
-        with urlopen("https://steamcommunity.com/app/" + str(self.steam_id)) as html:
-            data = str(html.read())
-        self.name = re.findall('<div class="apphub_AppName">(.*?)</div>', data)
-        
-        if '<div class="discount_final_price">' in data:
-            self.price = re.findall('<div class="discount_final_price">(.*?)</div>', data)
-        elif '<div class="game_purchase_price price">' in data:
-            self.price = re.findall('<div class="discount_final_price">(.*?)</div>', data)
-
-        tempDev = re.findall('<div class="summary column" id="developers_list"><a href="https://store.steampowered.com/developer/(.*?)</div>', data)
-        self.developer = re.findall('>(.*?</a>)', tempPub)
-
-        tempPub = re.findall('<div class="summary column"><a href="https://store.steampowered.com/publisher/(.*?)</div>', data)
-        self.publisher = re.findall('>(.*?)</a>', tempPub)
-
-        if '<b>Release Date:</b>' in data:
-            tempDate = re.findall('<b>Release Date:</b>(.*?)<br>', data)
-        elif '<b>Early Access Release Date:</b>' in data:
-            tempDate = re.findall('<b>Early Access Release Date:</b>(.*?)</b>', date)
-            
-        updateTime = re.sub(r'[^\w]', ' ', tempDate).split(" ")
-        self.releaseDate = datetime.date(int (updateTime[3], int (monthToNumber(updateTime[1])), int (updateTime[0]))
-        """
-
 
 
 deb = Game(107410)
