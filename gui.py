9f9191d9a74b1a569ccf41d46ce0f382cd979d82
Jakub Kohoutek <62189043+JustAKuba@users.noreply.github.com>
2021-05-09 13:37:40 +0100

    Created: Basic UI structure

--- gui.py
+++ gui.py
@@ -2,83 +2,68 @@ import tkinter as tk
 
 class Window:
 
+
     def __init__(self, title, ):
         "This class represents Graphical User Interface object"
         self.title = title
 
         self.gameCount = 0 #Number of loaded games in a window
 
-        pass
 
     def initialize(self):
         "Defines window itself"
         self.window = tk.Tk()
         self.window.title(self.title)
-        self.window.geometry("500x400+700+400")
+        self.window.geometry("500x600")
 
-        self.controls = tk.Frame(self.window)
-        self.data = tk.Frame(self.window)
-        self.controls.grid(column = 0)
-        self.data.grid(column = 1)
+        self.controls = tk.LabelFrame(self.window, text = "Control panel")
+        self.data = tk.LabelFrame(self.window, text = "Games")
+
+
+
+        self.controls.pack(fill = "both", expand = "yes", padx = 10, pady = 10, anchor = "w")
+        self.data.pack(fill = "both", expand = "yes", padx = 10, pady = 10, anchor = "w")
+        
+        canvas = tk.Canvas(self.data)
+        canvas.pack(side="left")
+
+        yscrollbar = tk.Scrollbar(self.data, orient = "vertical", command = canvas.yview)
+        yscrollbar.pack(side="right", fill="y")
+        
+        self.dataFrame = tk.Frame(canvas)
+        self.dataFrame.pack()
+
 
     def add_game(self, name, price, publisher, developer, releaseDate):
         "Puts the game information into the UI"
 
-        game = tk.Frame(self.data)
+        game = tk.LabelFrame(self.dataFrame)
+        game.pack( fill = "both")
         self.gameCount += 1
 
-        truName = tk.Label(text = name)
+        finName = tk.Label(game, text = name, anchor = "w")
 
-        tagPrice = tk.Label(text = "Price:")
-        truPrice = tk.Label(text = price)
-
-        tagPublisher = tk.Label(text = "Publisher:")
-        truPublisher = tk.Label(text = publisher)
-
-        tagDeveloper = tk.Label(text = "Developer:")
-        truDeveloper = tk.Label(text = developer)
-
-        tagRelease = tk.Label(text = "Release:")
-        truRelease = tk.Label(text = releaseDate)
+        finPrice = tk.Label(game, text = "Price: " + str(price), font=("Courier", 9))
+        finPublisher = tk.Label(game, text = "Publisher " + str(publisher), font=("Courier", 9))
+        finDeveloper = tk.Label(game, text = "Developer: " + str(developer), font=("Courier", 9))
+        finRelease = tk.Label(game, text = "Release date: " + str(releaseDate), font=("Courier", 9))
 
         constructionList = [
-            [tagPrice, truPrice],
-            [tagDeveloper, truDeveloper],
-            [tagPublisher, truPublisher],
-            [tagRelease, truRelease]
+            finPrice,
+            finPublisher,
+            finDeveloper,
+            finRelease
         ]
 
-        truName.grid(column = 0, row = 0, columnspan = 3)
+        finName.pack()
 
-        elRow = 0
+        segCount = 0
         for segment in constructionList:
-            elRow += 1
-            elColumn = 0
-            for element in segment:
-                secondSpan = 1
-                if elColumn % 2 == 0:
-                    secondSpan = 2
-               
-                element.grid(row = elRow, column = elColumn, columnspan = secondSpan, sticky = "w")
-                elColumn += 1
-                
-
-
-
-
-    def add_control(self):
-        "Adds element to the control panel."
-        pass
-
-
-
-
+            segment.pack(anchor = "w")
 
 
     def show(self):
         self.window.mainloop()
-
-
 
 
 
