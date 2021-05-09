f1bea21a67a63fb662cf5dbb6e00f74897701abd
Jakub Kohoutek <62189043+JustAKuba@users.noreply.github.com>
2021-05-09 21:18:36 +0100

    Created: Question popout window

--- gui.py
+++ gui.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
+import steamRecord
+
+
 
 class Window:
 
@@ -7,38 +11,47 @@ class Window:
         "This class represents Graphical User Interface object"
         self.title = title
 
-        self.gameCount = 0 #Number of loaded games in a window
-
 
     def initialize(self):
         "Defines window itself"
+        #Configuring master window
         self.window = tk.Tk()
         self.window.title(self.title)
         self.window.geometry("500x600")
 
-        self.controls = tk.LabelFrame(self.window, text = "Control panel")
+        self.master = self.window
+
+        #Preparing main sections
+        self.controls = tk.LabelFrame(self.window)
         self.data = tk.LabelFrame(self.window, text = "Games")
-
-
-
+       
+       
+        #Packing main sections
         self.controls.pack(fill = "both", expand = "yes", padx = 10, pady = 10, anchor = "w")
         self.data.pack(fill = "both", expand = "yes", padx = 10, pady = 10, anchor = "w")
         
+        #Creating canvas for scrolling frame feature
         canvas = tk.Canvas(self.data)
-        canvas.pack(side="left")
+        canvas.pack(side="left",fill = "both", expand = "yes")
 
+        #Configuring scrollbar
         yscrollbar = tk.Scrollbar(self.data, orient = "vertical", command = canvas.yview)
         yscrollbar.pack(side="right", fill="y")
-        
+
+        #Attaching scrollbar to the canvas
+        canvas.configure(yscrollcommand = yscrollbar.set)   
+        canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion = canvas.bbox('all')))
+
+        #Creating frame inside the canvas
         self.dataFrame = tk.Frame(canvas)
-        self.dataFrame.pack()
+        canvas.create_window((0,0), window=self.dataFrame, anchor = "nw")
 
 
     def add_game(self, name, price, publisher, developer, releaseDate):
         "Puts the game information into the UI"
 
         game = tk.LabelFrame(self.dataFrame)
-        game.pack( fill = "both")
+        game.pack( fill = "both", expand = "yes")
         self.gameCount += 1
 
         finName = tk.Label(game, text = name, anchor = "w")
@@ -61,14 +74,58 @@ class Window:
         for segment in constructionList:
             segment.pack(anchor = "w")
 
+    def add_controls(self):
+        "Fills the control section"
+        addGameBut = tk.Button(self.controls, text = "Add game using ID", command= lambda: self.newGameWindow(reqGameWin, "Add Game")).pack()
+        
+        pass
+
+
 
     def show(self):
+        "Closes the window loop."
         self.window.mainloop()
 
+    def newGameWindow(self, _class, title):
+        self.new = tk.Toplevel(self.master)
+        _class(self.new, title)
+        
+
+class reqGameWin:
+
+    def __init__(self, master, title):
+        self.master = master
+        self.master.geometry("400x400+200+200")
+        self.frame = tk.LabelFrame(self.master)
+        self.frame.pack()
+
+        self.entryString = tk.StringVar()
+        self.entr = tk.Entry(self.frame, textvariable = self.entryString)
+        
+        self.submit = tk.Button(self.frame, text = "Save", command =self.finishSubmit)
+        
+        self.entr.pack()
+        self.submit.pack()
+        
+
+    def finishSubmit(self):
+        submitString = self.entryString.get()
+        
+        submittedGame = steamRecord.Game(submitString)
+        submittedGame.save()
+
+        self.master.destroy()
 
 
-class Popup:
 
-    def __init__(self):
-        pass
+
+
+        
+def error(title, text):
+    messagebox.showerror(title, text)
+    
+
+
+def win():
+    """Open window"""
     
